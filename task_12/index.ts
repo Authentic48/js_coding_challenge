d164b443aed7813d15007f2c7625911f655f9acf
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-31 16:26:15 +0000

    color code done

--- task_12/index.ts
+++ task_12/index.ts
@@ -1,11 +1,8 @@
+//Leap year check
 export const isLeap = (year: number): boolean => {
-  if (year % 2 === 0) {
-    return true;
-  }
+  if (year % 4 === 0 && year % 100 != 0) return true;
 
-  if (year % 100 === 0) {
-    if (year % 400 === 0) return true;
-  }
+  if (year % 400 === 0) return true;
 
   return false;
 };
